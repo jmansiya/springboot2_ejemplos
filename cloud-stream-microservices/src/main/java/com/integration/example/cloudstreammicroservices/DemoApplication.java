e49d9db2a6b8d58f0f754d3741b33cb6d3b593e5
Jose Mansilla <jmansiya@users.noreply.github.com>
2019-06-08 00:25:43 +0200

    eliminamos properties y ponemos yml

--- cloud-stream-microservices/src/main/java/com/integration/example/cloudstreammicroservices/DemoApplication.java
+++ cloud-stream-microservices/src/main/java/com/integration/example/cloudstreammicroservices/DemoApplication.java
@@ -2,12 +2,22 @@ package com.integration.example.cloudstreammicroservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.messaging.handler.annotation.SendTo;
 
 @SpringBootApplication
+@EnableBinding(Processor.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@StreamListener(Processor.INPUT)
+	@SendTo(Processor.OUTPUT)
+	public String process(String s) {
+		return s.toUpperCase();
+	}
 }
