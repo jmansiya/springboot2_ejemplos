c4a81726bda1a82a5e9ab62ad67b3ddf4231b311
Jose Mansilla <jmansiya@users.noreply.github.com>
2019-06-08 00:00:25 +0200

    Spring Cloud Stream TEST
    Subida inicial proyecto pruebas con Spring Cloud Stream y Kafka.

---
+++ cloud-stream-microservices/src/test/java/com/integration/example/cloudstreammicroservices/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.integration.example.cloudstreammicroservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
